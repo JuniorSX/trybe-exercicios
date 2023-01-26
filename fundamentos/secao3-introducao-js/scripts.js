871a5467065488425c261ea5a4aab60782de8f36
Junior Souza <souza.jr91@gmail.com>
2023-01-26 15:25:58 -0300

    exercicios dia 4 js

--- fundamentos/secao3-introducao-js/scripts.js
+++ fundamentos/secao3-introducao-js/scripts.js
@@ -227,3 +227,24 @@ idade >= 18 ? console.log(pessoa + " é maior de idade") : console.log("Não"); 
 // 5 - Crie um algoritmo que recebe a idade de Carolzita , Flavio e Noel e imprime quem é a pessoa mais nova no formato:
 // "Pessoa" é a mais nova.
 
+//Crie uma variável player e atribua um objeto contendo as variáveis listadas abaixo:
+
+
+let player = { 
+    name:'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: { 
+        golden: 2, 
+        silver: 3 
+    }
+
+}
+player['fullName'] = player.name + ' ' + player.lastName;
+player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018]
+
+console.log('A jogadora ' + player['fullName'] + ' tem ' + player.age + ' anos de idade');
+// Acesse as chaves name, lastName e age, usando a sintaxe meuObjeto.chave, e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: “A jogadora Marta Silva tem 34 anos de idade”.
+
+console.log('A jogadora ' + player['fullName'] + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes');
+console.log('A jogadora possui ' + player.medals['golden'] + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata' );
